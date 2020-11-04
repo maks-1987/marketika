6f91b3db559f129ace2b7b0928a41deed462985c
MaxD <max.moderation@gmail.com>
2020-11-04 19:01:25 +0200

    to fix section-plans

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let section_plans = document.getElementById('section-plans__list-item');
+section_plans.addEventListener('click', activeColor);
+
+function activeColor() {
+    alert('activeColor')
+}
